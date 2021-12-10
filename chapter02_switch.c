646dc9850f406cdb6eb51f2facb2579851a0f2f7
vinsonw <me@vinsonwei.com>
2021-12-11 03:42:24 +0800

    reformat

--- chapter02_switch.c
+++ chapter02_switch.c
@@ -6,26 +6,26 @@ int main(void)
     const int two=4;
     switch (i)
     {
-    case 1:
-        printf("1\n");
-        break;
+        case 1:
+            printf("1\n");
+            break;
 
-    // Here lies a bug:
-    //     error: case label does not reduce to an integer constant
-    //    12 |     case two:
-    //       |     ^~~~ 
-    case two:
-        printf("here is two.\n");
-        break;
-    case 2:
-        printf("2\n");
-        break;
-    case 3:
-        printf("3\n");
-        break;
-    
-    default:
-        printf("default!\n");
-        break;
+        // Here lies a bug:
+        //     error: case label does not reduce to an integer constant
+        //    12 |     case two:
+        //       |     ^~~~ 
+        case two:
+            printf("here is two.\n");
+            break;
+        case 2:
+            printf("2\n");
+            break;
+        case 3:
+            printf("3\n");
+            break;
+        
+        default:
+            printf("default!\n");
+            break;
     }
 }
